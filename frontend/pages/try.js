88378bf2818be93ffac8addadd1d932facfe6393
ayong <0kay0802@gmail.com>
2024-07-04 18:02:00 +0900

    try

--- frontend/pages/try.js
+++ frontend/pages/try.js
@@ -1,17 +1,17 @@
-import { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import ReactAudioPlayer from 'react-audio-player';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import styles from '../styles/Try.module.css';
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import ReactAudioPlayer from "react-audio-player";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import styles from "../styles/Try.module.css";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function TryVoice() {
   const [file, setFile] = useState(null);
-  const [result, setResult] = useState('');
-  const [activeTab, setActiveTab] = useState('celebrity'); // 'celebrity' or 'phishing'
+  const [result, setResult] = useState("");
+  const [activeTab, setActiveTab] = useState("celebrity"); // 'celebrity' or 'phishing'
   const [playingCelebrity, setPlayingCelebrity] = useState(null);
   const [playingFake, setPlayingFake] = useState(null);
   const [isPlayingCelebrity, setIsPlayingCelebrity] = useState(false);
@@ -22,10 +22,10 @@ export default function TryVoice() {
   const fakeAudioRef = useRef(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) {
-      alert('You must be logged in to access this page');
-      router.push('/login');
+      alert("You must be logged in to access this page");
+      router.push("/login");
     }
   }, [router]);
 
@@ -36,25 +36,30 @@ export default function TryVoice() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!file) {
-      alert('Please select a file');
+      alert("Please select a file");
       return;
     }
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
     try {
-      const token = localStorage.getItem('token');
-      const response = await axios.post(`${BACKEND_URL}/api/upload-audio/`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'Authorization': `Token ${token}`
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        `${BACKEND_URL}/api/upload-audio/`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Token ${token}`,
+          },
         }
-      });
+      );
       const analysisResult = response.data.analysis_result;
-      const message = analysisResult === 1 ? '음성 파일입니다.' : '음성 파일이 아닙니다.';
+      const message =
+        analysisResult === 1 ? "음성 파일입니다." : "음성 파일이 아닙니다.";
       setResult(message);
     } catch (error) {
-      console.error('Error uploading file', error);
-      alert('Error uploading file');
+      console.error("Error uploading file", error);
+      alert("Error uploading file");
     }
   };
 
@@ -118,79 +123,34 @@ export default function TryVoice() {
 
   return (
     <div className={styles.previewContext}>
-      <Navbar />
       <div className={styles.mainContent}>
-        <h1>Try Voice Verity</h1>
-        <div className={styles.tabButtons}>
-          <button className={activeTab === 'celebrity' ? styles.activeTab : ''} onClick={() => handleTabChange('celebrity')}>Celebrity Deep Fake</button>
-          <span> | </span>
-          <button className={activeTab === 'phishing' ? styles.activeTab : ''} onClick={() => handleTabChange('phishing')}>Phishing Detection</button>
+        <Navbar />
+        <div className={styles.block}>
+          <div style={{height:"100px"}} />
+          <h1>Try Voice Verity</h1>
+          <h2>
+            Voice Verity에서는 실시간 통화 중 딥보이스를 감지할 수 있습니다.
+          </h2>
+          <h2>Fake Voice를 탐지하는 순간을 체험해보세요.</h2>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.form}>
+            <label for="upload"></label>
+            <input id='upload' type="file" onChange={handleFileChange}
+            className={styles.uploadhidden}></input>
+            </div>
+            <h2>음성파일을 넣은 뒤, Generate 버튼을 눌러주세요</h2>
+            <p style={{color:"#666"}}>10MB 이내의 음성 파일로 제한(파일: .wav, .mp3, .mp4)</p>
+            <button type="submit">▶ Start Detection</button>
+          </form>
+          <div className={styles.resultContext}>
+            {result && (
+              <div>
+                <h2>Analysis Result:</h2>
+                <p>{result}</p>
+              </div>
+            )}
+          </div>
         </div>
-        {activeTab === 'celebrity' ? (
-          <>
-            <h2 className={styles.fontsize}>Show what we do</h2>
-            <h2>The Dangers of Deep Voice</h2>
-            <div className={styles.fileupload}>
-              <div className={styles.playcontent}>
-                <div className={styles.pbox}>
-                  <div className={styles.profile}>
-                    <div className={styles.eek}>
-                      <div className={styles.faceplay} onMouseEnter={() => playCelebrityAudio('/audios/try/celebrity1.wav')}></div>
-                    </div>
-                  </div>
-                  <div className={styles.play}><button onClick={() => playFakeAudio('/audios/try/fake1.wav')}></button></div>
-                </div>
-                <div className={styles.pbox}>
-                  <div className={styles.profile}>
-                    <div className={styles.juj}>
-                      <div className={styles.faceplay} onMouseEnter={() => playCelebrityAudio('/audios/try/celebrity1.wav')}></div>
-                    </div>
-                  </div>
-                  <div className={styles.play}><button onClick={() => playFakeAudio('/audios/try/fake1.wav')}></button></div>
-                </div>
-              </div>
-              {playingCelebrity && (
-                <ReactAudioPlayer src={playingCelebrity} ref={celebAudioRef} controls style={{ display: 'none' }} />
-              )}
-              {playingFake && (
-                <ReactAudioPlayer src={playingFake} ref={fakeAudioRef} controls style={{ display: 'none' }} />
-              )}
-            </div>
-            <h2>Upload your voice</h2>
-            <div className={styles.fileupload}>
-              <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileChange} />
-                <button type="submit">Upload</button>
-              </form>
-              <div className={styles.resultContext}>
-                {result && (
-                  <div>
-                    <h2>Analysis Result:</h2>
-                    <p>{result}</p>
-                  </div>
-                )}
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <h2 className={styles.fontsize}>Detect Phishing Attempts</h2>
-            <div className={styles.fileupload}>
-              <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileChange} />
-                <button type="submit">Upload</button>
-              </form>
-              <div className={styles.resultContext}>
-                {result && (
-                  <div>
-                    <h2>Analysis Result:</h2>
-                    <p>{result}</p>
-                  </div>
-                )}
-              </div>
-            </div>
-          </>
-        )}
       </div>
       <Footer />
     </div>
