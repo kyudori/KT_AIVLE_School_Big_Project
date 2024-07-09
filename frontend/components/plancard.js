e72d86dd20e38b2b7c82d4c5eeb077fe39598135
Han Kyuhyun <khh5345@naver.com>
2024-07-09 13:05:19 +0900

    cors 수정
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- frontend/components/plancard.js
+++ frontend/components/plancard.js
@@ -23,6 +23,7 @@ const PlanCard = ({ plan }) => {
           headers: {
             Authorization: `Token ${token}`,
           },
+          withCredentials: true, // 세션 인증을 위해 필요
         }
       );
       const { next_redirect_pc_url, tid } = response.data;
